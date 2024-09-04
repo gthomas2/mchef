392c5fdcd7bac9938f02454ef94b784dffee5ca0
Julian Vecera <hello@julianvecera.com>
2024-09-04 12:56:17 +0200

    add Recipe Parser Service Tests

--- src/Tests/RecipeParserServiceTest.php
+++ src/Tests/RecipeParserServiceTest.php
@@ -1,114 +1,126 @@
 <?php declare(strict_types=1);
 
-use App\Model\Plugin;
-use App\Model\Volume;
-use App\Model\PluginsInfo;
 use App\Model\Recipe;
+use App\Service\RecipeParser;
 use PHPUnit\Framework\TestCase;
-use App\Service\Plugins;
-use \App\Traits\CallRestrictedMethodTrait;
+use splitbrain\phpcli\Exception;
 
-final class PluginsServiceTest extends TestCase {
-    use CallRestrictedMethodTrait;
+final class RecipeParserServiceTest extends TestCase {
+    
+    private $validJson = '
+    {
+      "name": "example",
+      "moodleTag": "v4.1.0",
+      "phpVersion": "8.0",
+      "plugins": ["https://github.com/gthomas2/moodle-moodle-filter_imageopt"],
+      "containerPrefix": "imageopt",
+      "host": "moodle-image-opt.test",
+      "port": 80,
+      "updateHostHosts": false,
+      "dbType": "pgsql",
+      "developer": true
+    }';
 
-    public function testGetMoodlePluginPath(): void {
-        $mockCli = $this->createMock(splitbrain\phpcli\CLI::class);
-        $pluginsService = Plugins::instance($mockCli);
-        $path = $this->callRestricted($pluginsService, 'getMoodlePluginPath', ['local_test']);
-        $this->assertEquals('/local/test', $path);
+    public function testParseValidRecipe(): void {
+        $filePath = tempnam(sys_get_temp_dir(), 'recipe_');
+        file_put_contents($filePath, $this->validJson);
+
+        $recipeParser = RecipeParser::instance();
+        $recipe = $recipeParser->parse($filePath);
+
+        $this->assertInstanceOf(Recipe::class, $recipe);
+        $this->assertEquals('v4.1.0', $recipe->moodleTag);
+        $this->assertEquals('8.0', $recipe->phpVersion);
+        $this->assertEquals('moodle-image-opt.test', $recipe->host);
+        $this->assertEquals(80, $recipe->port);
+        $this->assertEquals('pgsql', $recipe->dbType);
+
+        unlink($filePath);
     }
 
-    //public function testGetPluginsInfoFromRecipe(): void {
-    //    $mockCli = $this->createMock(splitbrain\phpcli\CLI::class);
-    //    $pluginsService = Plugins::instance($mockCli);
-    //    $recipeService = \App\Service\RecipeParser::instance();
-    //    $recipe = $recipeService->parse(__DIR__.'/Fixtures/test-mrecipe.json');
-    //    var_dump($pluginsService->getPluginsInfoFromRecipe($recipe));
-    //
+    public function testParseInvalidJson(): void {
+        $filePath = tempnam(sys_get_temp_dir(), 'recipe_');
+        file_put_contents($filePath, 'invalid json');
+
+        $recipeParser = RecipeParser::instance();
+
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Failed to decode recipe JSON');
+        $recipeParser->parse($filePath);
+
+        unlink($filePath);
+    }
+
+    public function testParseMissingFile(): void {
+        $filePath = '/path/to/nonexistent/recipe.json';
+        $recipeParser = RecipeParser::instance();
+
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Recipe file does not exist');
+        $recipeParser->parse($filePath);
+    }
+
+    public function testParseMissingRequiredProperty(): void {
+        $invalidJson = '
+        {
+          "name": "example",
+          "phpVersion": "8.0"
+        }';
+        
+        $filePath = tempnam(sys_get_temp_dir(), 'recipe_');
+        file_put_contents($filePath, $invalidJson);
+
+        $recipeParser = RecipeParser::instance();
+
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Missing property in recipe');
+        $recipeParser->parse($filePath);
+
+        unlink($filePath);
+    }
+
+    //public function testParseUnsupportedPhpVersion(): void {
+    //    $invalidJson = '
+    //    {
+    //      "name": "example",
+    //      "moodleTag": "v4.1.0",
+    //      "phpVersion": "7.0"
+    //    }';
+    //    
+    //    $filePath = tempnam(sys_get_temp_dir(), 'recipe_');
+    //    file_put_contents($filePath, $invalidJson);
+//
+    //    $recipeParser = RecipeParser::instance();
+//
+    //    $this->expectException(Exception::class);
+    //    $this->expectExceptionMessage('Unsupported php version');
+    //    $recipeParser->parse($filePath);
+//
+    //    unlink($filePath);
     //}
 
-    public function testGetPluginComponentFromVersionFile(): void {
-        $mockCli = $this->createMock(splitbrain\phpcli\CLI::class);
-        $pluginsService = Plugins::instance($mockCli);
-        $component = $pluginsService->getPluginComponentFromVersionFile(__DIR__.'/Fixtures/version-test.php');
-        $this->assertEquals('mod_assign', $component);
-    }
+    public function testSetDefaults(): void {
+        $recipeParser = RecipeParser::instance();
+        
+        $json = '
+        {
+          "name": "example",
+          "moodleTag": "v4.1.0",
+          "phpVersion": "8.0",
+          "host": "moodle-image-opt.test",
+          "includeBehat": true
+        }';
+        
+        $filePath = tempnam(sys_get_temp_dir(), 'recipe_');
+        file_put_contents($filePath, $json);
 
-    public function testFindMoodleVersionFiles(): void {
-        $mockCli = $this->createMock(splitbrain\phpcli\CLI::class);
-        $pluginsService = Plugins::instance($mockCli);
-    
-        $tempDir = sys_get_temp_dir() . '/moodle_test_' . uniqid();
-        mkdir($tempDir);
-        mkdir($tempDir . '/subdir1');
-        mkdir($tempDir . '/subdir2');
-        file_put_contents($tempDir . '/version.php', '<?php $plugin->version = 2022020801;');
-        file_put_contents($tempDir . '/subdir1/version.php', '<?php $plugin->version = 2022020800;');
-        file_put_contents($tempDir . '/subdir2/otherfile.php', '<?php // not a version file');
-    
-        $versionFiles = $pluginsService->findMoodleVersionFiles($tempDir);
-    
-        unlink($tempDir . '/version.php');
-        unlink($tempDir . '/subdir1/version.php');
-        unlink($tempDir . '/subdir2/otherfile.php');
-        rmdir($tempDir . '/subdir1');
-        rmdir($tempDir . '/subdir2');
-        rmdir($tempDir);
-    
-        $expectedFiles = [
-            $tempDir . '/version.php',
-            $tempDir . '/subdir1/version.php'
-        ];
-        sort($versionFiles);
-        sort($expectedFiles);
-        $this->assertEquals($expectedFiles, $versionFiles);
-    }
+        $recipe = $recipeParser->parse($filePath);
 
-    public function testGetPluginByComponentName(): void {
-        $mockCli = $this->createMock(splitbrain\phpcli\CLI::class);
-        $pluginsService = Plugins::instance($mockCli);
-    
-        $plugin1 = new Plugin(
-            'mod_assign',
-            '/path/to/mod_assign',
-            '/full/path/to/mod_assign',
-            $this->createMock(Volume::class),
-            'https://github.com/moodle/mod_assign',
-            Plugin::TYPE_SINGLE
-        );
-    
-        $plugin2 = new Plugin(
-            'mod_forum',
-            '/path/to/mod_forum',
-            '/full/path/to/mod_forum',
-            $this->createMock(Volume::class),
-            'https://github.com/moodle/mod_forum',
-            Plugin::TYPE_SINGLE
-        );
-    
-        $pluginsInfo = new PluginsInfo(
-            [], // Pass an empty array or mock volumes here
-            [$plugin1, $plugin2]
-        );
-    
-        $result = $pluginsService->getPluginByComponentName('mod_assign', $pluginsInfo);
-        $this->assertSame($plugin1, $result);
-    
-        $result = $pluginsService->getPluginByComponentName('mod_nonexistent', $pluginsInfo);
-        $this->assertNull($result);
-    
-        $pluginsInfo->plugins[] = new Plugin(
-            'mod_assign',
-            '/path/to/another_mod_assign',
-            '/full/path/to/another_mod_assign',
-            $this->createMock(Volume::class),
-            'https://github.com/moodle/another_mod_assign',
-            Plugin::TYPE_SINGLE
-        );
-    
-        $this->expectException(Exception::class);
-        $this->expectExceptionMessage('Found more than one plugin entry for component "mod_assign" this should not happen');
-        $pluginsService->getPluginByComponentName('mod_assign', $pluginsInfo);
+        $this->assertEquals('http://moodle-image-opt.test', $recipe->wwwRoot);
+        $this->assertEquals('moodle-image-opt.test.behat', $recipe->behatHost);
+        $this->assertEquals('http://moodle-image-opt.test.behat', $recipe->behatWwwRoot);
+        $this->assertEquals('mc-moodle', $recipe->dbName);
+
+        unlink($filePath);
     }
-    
 }
